65afa3f7812a3a6050c3670626b55f17fa3b576e
Seva <zimovetsva@gmail.com>
2025-11-29 22:57:17 +0400

    Вывод ошибок переведен на русский язык

--- lab_4/config.py
+++ lab_4/config.py
@@ -6,16 +6,20 @@ class Config:
         self.key_len = settings_dict.get('key_len')
 
     def get_path(self, key: str) -> str:
-        """Безопасное получение пути"""
+        """
+        Безопасное получение пути
+        :param key: Имя поля для получения пути
+        :return:
+        """
         path = self.paths.get(key)
         if not path:
-            raise ValueError(f"Missing path: {key}")
+            raise ValueError(f"Отсутствует путь: {key}")
         return path
 
     def get_key_len(self) -> str:
         """Проверка наличия и значения key_len"""
         if not self.key_len:
-            raise ValueError("key_len is required")
+            raise ValueError("Необходимо задать key_len")
         if self.key_len not in ('128', '192', '256'):
             raise ValueError("Недопустимая длина ключа. "
                              "Допустимые значения: '128', '192', '256'")
