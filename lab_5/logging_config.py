79bd037ee0576336bedd30b346f07bf390311079
Seva <zimovetsva@gmail.com>
2025-12-14 08:21:52 +0400

    Вывод логов теперь идёт в файл

--- lab_5/logging_config.py
+++ lab_5/logging_config.py
@@ -12,12 +12,16 @@ def setup_logging():
         '%(asctime)s - %(levelname)-8s - %(name)-20s - %(message)s',
         datefmt='%H:%M:%S'
     )
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(formatter)
-    console_handler.setLevel(logging.INFO)
+    file_handler = logging.FileHandler(
+        filename='lab_logs.log',
+        mode='a',
+        encoding='utf-8'
+    )
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.INFO)
     root_logger = logging.getLogger()
     root_logger.handlers = []
-    root_logger.addHandler(console_handler)
+    root_logger.addHandler(file_handler)
     root_logger.setLevel(logging.INFO)
     root_logger.propagate = True
     return root_logger
