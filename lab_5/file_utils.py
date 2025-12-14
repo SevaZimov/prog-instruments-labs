49829b05cefa3ee6570dfaf9b1d2ae39cd793e9a
Seva <zimovetsva@gmail.com>
2025-12-14 08:22:29 +0400

    Добавлено логирование ошибок в file_utils

--- lab_5/file_utils.py
+++ lab_5/file_utils.py
@@ -16,6 +16,7 @@ def save_binary(path: str, data: bytes) -> None:
             f.write(data)
         logger.info(f"Файл успешно сохранен: {path}")
     except (OSError, TypeError) as e:
+        logger.error(f"Ошибка сохранения файла: {e}")
         raise IOError(f"Ошибка сохранения файла: {e}")
 
 def read_binary(path: str) -> bytes:
@@ -26,6 +27,7 @@ def read_binary(path: str) -> bytes:
     """
     logger.info(f"Чтение бинарного файла: {path}")
     if not os.path.exists(path):
+        logger.error(f"Файл не найден: {path}")
         raise FileNotFoundError(f"Файл не найден: {path}")
     with open(path, 'rb') as f:
         return f.read()
@@ -43,6 +45,7 @@ def save_txt(path: str, content: str) -> None:
             f.write(content)
         logger.info(f"Текстовый файл успешно сохранен: {path}")
     except (OSError, TypeError) as e:
+        logger.error(f"Ошибка сохранения файла: {e}")
         raise IOError(f"Ошибка сохранения файла: {e}")
 
 def read_txt(path: str) -> str:
@@ -53,6 +56,7 @@ def read_txt(path: str) -> str:
     """
     logger.info(f"Чтение текстового файла: {path}")
     if not os.path.exists(path):
+        logger.error(f"Файл не найден: {path}")
         raise FileNotFoundError(f"Файл не найден: {path}")
     with open(path, 'r', encoding='utf-8') as f:
         return f.read()
